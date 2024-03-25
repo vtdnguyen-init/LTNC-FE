10ebef9150d7fb1009e5f391c086d16bf8389409
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2024-03-25 18:06:16 +0700

    add Footer

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -2,7 +2,7 @@
 import React, { useState, ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
-
+import Footer from "@/components/Footer";
 export default function DefaultLayout({
   children,
 }: {
@@ -18,19 +18,22 @@ export default function DefaultLayout({
         {/* <!-- ===== Sidebar End ===== --> */}
 
         {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <div className="relative flex h-screen flex-1 flex-col place-content-between overflow-y-auto overflow-x-hidden">
           {/* <!-- ===== Header Start ===== --> */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}
 
           {/* <!-- ===== Main Content Start ===== --> */}
           <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            <div className="mx-auto max-w-screen-2xl place-items-start p-4 md:p-6 2xl:p-10">
               {children}
             </div>
           </main>
+
           {/* <!-- ===== Main Content End ===== --> */}
+          <Footer />
         </div>
+
         {/* <!-- ===== Content Area End ===== --> */}
       </div>
       {/* <!-- ===== Page Wrapper End ===== --> */}
